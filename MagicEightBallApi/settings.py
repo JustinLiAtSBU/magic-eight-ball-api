a5dbb2a412a768053f79719d54c61ae38f2625db
Justin <justinli.9876@gmail.com>
2022-04-04 17:53:11 -0400

    Configure heroku

--- MagicEightBallApi/settings.py
+++ MagicEightBallApi/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-02b-e=j^d8yq+@@zh34#9jqkvfcid(_(nc%$h*(hj5%ccurhpw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
